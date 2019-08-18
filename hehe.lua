6828fb235c8a02456bb77c7a8592b3d7917911fc
Gabriel <drgabi18@yahoo.co.uk>
2019-08-18 15:52:19 +0300

    i removed the IJustFlippedTheSwitch part actually
    just posted it for archival purposes

--- hehe.lua
+++ hehe.lua
@@ -1,23 +1,33 @@
 function Initialize()
-	if SELF:GetNumberOption('IJustFlippedTheSwitch', '0') == 0 then else SKIN:Bang('!SetOption', 'Color', 'String', '[&Lua:f]'); SKIN:Bang('!SetOption', 'SubColor', 'String', '[R][G][B]') end
-	Update()
-end
+	local function LeOption(LeMeasure, LeObject)           return SKIN:GetMeasure(LeMeasure):GetOption(LeObject)         end
+	local function LeSet(LeMeasure, LeOption, LeString)    return SKIN:Bang('!SetOption', LeMeasure, LeOption, LeString) end
 
-function Update()
-	function String(Muffet) return SKIN:GetMeasure(Muffet):GetStringValue() end
-	r,g,b = String('R'), String('G'), String('B')
-	Do(r..g..b)
+	am = string.gsub(SKIN:GetVariable('AccurateMultiplier', 1), ",", ".")
+
+	local a, b = LeOption('Color', 'String'), LeOption('SubColor', 'String')
+	if SELF:GetNumberOption('IJustFlippedTheSwitch', '0') == 0 then 
+		LeSet('Color',    'String', a)
+		LeSet('SubColor', 'String', b)
+	else 
+		LeSet('Color',    'String', b)
+		LeSet('SubColor', 'String', a)
+	end
+    
+	if SKIN:GetVariable('ShowBox', 1) ~= "1" then 
+		LeSet('TimeBox',  'SolidColor', '00000001')
+		LeSet('CopyText', 'SolidColor', '00000001')
+		LeSet('CopyTextAccurate', 'SolidColor', '00000001')
+	end
 end
 
 function Do(stuff)
-	local t={}
-
-	for i = 1,5,2 do table.insert(t, i, stuff:sub(i, i+1)) end
-	--1 2
-	--3 4
-	--5 6
-	local function Todd(ItJustWorks) return string.format("%02s", ItJustWorks) end
-	local a,b,c = Todd(string.format("%X", t[1])), Todd(string.format("%X", t[3])), Todd(string.format("%X", t[5]))
-	f = a..b..c
 	--steve ur sister is hot
+	local t={}
+	local function Todd(ItJustWorks) return string.format("%02s", ItJustWorks) end
+	--how the fuck
+	
+	for i = 1,5,2 do table.insert(t, i, stuff:sub(i, i+1)) end
+	--1 2, 3 4, 5 6
+	local a, b, c = Todd(string.format("%X", t[1] * am)), Todd(string.format("%X", t[3] * am)), Todd(string.format("%X", t[5] * am))
+	return a..b..c
 end
